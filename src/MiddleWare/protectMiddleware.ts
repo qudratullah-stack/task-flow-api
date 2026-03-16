ecfe9bdfc411f8c03fe90c424d778f5de911b070
qudratullah-stack <qudratullahmasoom6688@gmail.com>
2026-03-16 17:00:14 +0500

    feat(middleware): add protect middleware for route authorization
    Implemented JWT-based protection middleware to verify access tokens and populate req.user. Secured the logout route using the new protect middleware.

---
+++ src/MiddleWare/protectMiddleware.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import { signup } from "../Models/signupSchema";
+
+export const protect = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.SaasAccessToken;
+
+    if (!token) {
+        res.status(401);
+        throw new Error("Not authorized, please login first");
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN!) as any;
+        
+        (req as any).user = await signup.findById(decoded.id).select("-password");
+
+        next();
+    } catch (error) {
+        res.status(401);
+        throw new Error("Not authorized, token failed");
+    }
+});
--- src/Routers/gooGleAuthRouter.ts
+++ src/Routers/gooGleAuthRouter.ts
@@ -3,7 +3,10 @@ const googleRouter = express.Router()
 import { signupLimit } from '../MiddleWare/limitMiddle' 
 import { googleLogin , loginSuccess,googleCallback  } from '../Controllers/googleAuthController'
 import { refreshAccessToken } from '../Controllers/refreshAccessToken'
+import { logout } from '../Controllers/logoutController'
+import { protect } from '../MiddleWare/protectMiddleware'
 googleRouter.get('/google',signupLimit,googleLogin)
 googleRouter.get('/google/callback',googleCallback, loginSuccess)
 googleRouter.get('/refresh-token',refreshAccessToken)
+googleRouter.get('/logout',protect,logout)
 export default googleRouter;
